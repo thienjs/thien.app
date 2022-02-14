85ca45a0d2a60427c88a48b3e00470274268a5be
Thien Tran <81346696+thienjs@users.noreply.github.com>
2022-02-14 16:07:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nextjs, Typescript, Tailwind Starter
+
+## Features
+
+- ⚡️ Next.js 12
+- ⚛️ React 17
+- ✨ TypeScript
+- 💨 Tailwind CSS 3 
+- 📏 ESLint
+- 💖 Prettier
+- Jest
+- Auto Import
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/**/*.{js,ts,jsx,tsx,html,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,html,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
